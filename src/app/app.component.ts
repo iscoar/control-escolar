cf2a3e7cfd7c4dd0581321db8e51c1e13f438f6e
Isco Amador <isco.amaador@gmail.com>
2019-10-03 14:40:38 -0500

    Modificaciones para el login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  providers: [UserService]
 })
 export class AppComponent {
-  title = 'control-escolar';
+  public title = 'control-escolar';
+  public identity;
+  public token;
+
+  constructor(
+    public _userService: UserService
+  ){
+    this.loadUser();
+  }
+
+  ngOnInit() {
+    console.log('pagina cargada');
+  }
+
+  ngDoCheck() {
+    this.loadUser();
+  }
+
+  loadUser() {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { routing, appRoutingProvider } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
