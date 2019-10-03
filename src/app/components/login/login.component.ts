fed34723339036a1d1bf6bbcb34e36f7ea77210e
Isco Amador <isco.amaador@gmail.com>
2019-10-03 14:38:09 -0500

    Crear componentes de login, home y error

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+  public page_title: string;
+  public user: User;
+  public status: string;
+  public token;
+  public identity;
+
+  constructor(
+    private _userService: UserService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
+    this.page_title = 'Identificate';
+    this.user = new User(1, '', '', '', 'ROLE_STUDENT', '', '', '', '', '', '', '', '');
+  }
+
+  ngOnInit() {
+    this.logout();
+  }
+
+  onSubmit(form) {
+    this._userService.signup(this.user).subscribe(
+      response => {
+        if (response.status != 'error') {
+          this.status = 'success';
+          this.token = response;
+
+          this._userService.signup(this.user, true).subscribe(
+            response => {
+              this.identity = response;
+              console.log(this.token);
+              console.log(this.identity);
+
+              localStorage.setItem('token', this.token);
+              localStorage.setItem('identity', JSON.stringify(this.identity));
+
+              this._router.navigate(['inicio']);
+            },
+            error => {
+              this.status = 'error';
+              console.log(<any>error);
+            }
+          )
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        this.status = 'error';
+        console.log(<any>error);
+      }
+    )
+  }
+
+  logout() {
+    this._route.params.subscribe(params => {
+      let logout = +params['sure'];
+
+      if (logout == 1) {
+        localStorage.removeItem('identity');
+        localStorage.removeItem('token');
+
+        this.identity = null;
+        this.token = null;
+
+        this._router.navigate(['inicio']);
+      }
+    })
+  }
+
+}
