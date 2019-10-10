12bbaa8d2472acd16300b81a37fe7e263c78fcc2
Isco Amador <isco.amaador@gmail.com>
2019-10-10 14:38:08 -0500

    Modificar vista principal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,8 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  public items: any[];
+  public assetsUrl: string;
 
-  constructor() { }
+  constructor() {
+    this.assetsUrl = 'assets/img/Iconos/';
+    this.items = [
+      {
+        title: 'Avisos',
+        icon: 'fas fa-trash',
+        color: 'text-primary',
+        url: ''
+      },
+      {
+        title: 'Calificaciones',
+        icon: 'Alumno/Calificaciones.png',
+        color: 'deep-orange-text',
+        url: ''
+      },
+      {
+        title: 'Cuenta',
+        icon: 'fas fa-trash',
+        color: 'text-info',
+        url: ''
+      },
+      {
+        title: 'Historico',
+        icon: 'Alumno/Historico.png',
+        color: 'text-success',
+        url: ''
+      },
+      {
+        title: 'Perfil',
+        icon: 'Alumno/Perfil.png',
+        color: 'text-danger',
+        url: '/perfil'
+      },
+      {
+        title: 'Salir',
+        icon: 'fas fa-trash',
+        color: 'deep-purple-text',
+        url: ''
+      }
+    ]
+  }
 
   ngOnInit() {
   }
