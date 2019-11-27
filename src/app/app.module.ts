d4a89b5242c2b08832d8a7794e9ded41a3d3f7b2
Alfredo-Macias <Alfredo.concur@gmail.com>
2019-11-27 16:53:40 -0600

    Added frontend to students absences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { StudentsModule } from './students/students.module';
 import { TeachersModule } from './teachers/teachers.module';
 import { CashiersModule } from './cashiers/cashiers.module';
 import { WelcomeComponent } from './students/components/welcome/welcome.component';
+//import { GroupAttendanceComponent } from './teachers/components/group-attendance/group-attendance.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { WelcomeComponent } from './students/components/welcome/welcome.componen
     HomeComponent,
     ErrorComponent,
     HeaderComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    //HttpClientModule
+    //GroupAttendanceComponent
   ],
   imports: [
     BrowserModule,
